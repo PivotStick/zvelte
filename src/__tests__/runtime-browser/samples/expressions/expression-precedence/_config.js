87087e3d620b5d0319913b86d570a5aedceccb01
PivotStick <pivoorigami@gmail.com>
2024-06-12 00:09:36 +0200

    test: 🔨 added precedence tests

---
+++ src/__tests__/runtime-browser/samples/expressions/expression-precedence/_config.js
@@ -0,0 +1,26 @@
+import { expect } from "vitest";
+import { defineTest } from "../../../defineTest.js";
+
+export default defineTest({
+    get props() {
+        return {
+            out1: undefined,
+            out2: undefined,
+            out3: undefined,
+            out4: undefined,
+            out5: undefined,
+            out6: undefined,
+            out7: undefined,
+        };
+    },
+    test({ props }) {
+        expect(props.out1).toEqual(7);
+        expect(props.out2).toEqual(5);
+        expect(props.out3).toEqual(1.5);
+        expect(props.out4).toEqual(4.5);
+        expect(props.out5).toEqual(38.7);
+
+        expect(props.out6).toEqual(7);
+        expect(props.out7).toEqual(9);
+    },
+});
