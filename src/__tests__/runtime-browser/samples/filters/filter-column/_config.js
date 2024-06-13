4791df04a973321527b3cad8cafb55972bebf26e
PivotStick <pivoorigami@gmail.com>
2024-06-13 08:30:35 +0200

    test: 🧪 filter-column

--- src/__tests__/runtime-browser/samples/filters/filter-column/_config.js
+++ src/__tests__/runtime-browser/samples/filters/filter-column/_config.js
@@ -1,5 +1,12 @@
+import { expect } from "vitest";
 import { defineTest } from "../../../defineTest.js";
 
 export default defineTest({
-    todo: true,
+    props: {
+        items: [{ fruit: "apple" }, { fruit: "orange" }],
+        output: undefined,
+    },
+    test({ props }) {
+        expect(props.output).toEqual(["apple", "orange"]);
+    },
 });
