918964fe41e3a26b7e97c24f1407f74c7047b2c2
PivotStick <pivoorigami@gmail.com>
2024-06-13 08:57:47 +0200

    test: 🧪 filter-json-encode

--- src/__tests__/runtime-browser/samples/filters/filter-json-encode/_config.js
+++ src/__tests__/runtime-browser/samples/filters/filter-json-encode/_config.js
@@ -1,5 +1,19 @@
 import { defineTest } from "../../../defineTest.js";
 
+const item = {
+    foo: true,
+    bar: "hello",
+    stuffs: [1, 2, 3],
+    user: {
+        firstName: "joe",
+        lastName: "doe",
+    },
+};
+
 export default defineTest({
-    todo: true,
+    props: {
+        item,
+    },
+
+    html: JSON.stringify(item),
 });
