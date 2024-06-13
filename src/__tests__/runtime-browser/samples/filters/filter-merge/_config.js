c82e0a0ee993f09ca45b646787551a1601e7123b
mdinatale <mdinatale@encaps.io>
2024-06-13 18:38:52 +0200

    test: 🧪 filter-merge

--- src/__tests__/runtime-browser/samples/filters/filter-merge/_config.js
+++ src/__tests__/runtime-browser/samples/filters/filter-merge/_config.js
@@ -1,5 +1,28 @@
+import { expect } from "vitest";
 import { defineTest } from "../../../defineTest.js";
 
 export default defineTest({
-    todo: true,
+    get props() {
+        return {
+            values: [1, 2],
+            items: {
+                apple: "fruit",
+                orange: "fruit",
+                peugeot: "unknown",
+            },
+
+            output1: undefined,
+            output2: undefined,
+        };
+    },
+
+    test({ props }) {
+        expect(props.output1).toEqual([1, 2, "apple", "orange"]);
+        expect(props.output2).toEqual({
+            apple: "fruit",
+            orange: "fruit",
+            peugeot: "car",
+            renault: "car",
+        });
+    },
 });
