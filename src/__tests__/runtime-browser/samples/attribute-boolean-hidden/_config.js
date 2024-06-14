c1011ecd367122c58565ae9e41ae25b3ca4f250b
PivotStick <pivoorigami@gmail.com>
2024-06-15 01:30:39 +0200

    test: 🧪 attribute-bool-false

--- src/__tests__/runtime-browser/samples/attribute-boolean-hidden/_config.js
+++ src/__tests__/runtime-browser/samples/attribute-boolean-hidden/_config.js
@@ -1,5 +1,17 @@
+import { expect } from "vitest";
+import { tick } from "../../../../internal/client/index.js";
 import { defineTest } from "../../defineTest.js";
 
 export default defineTest({
-    todo: true,
+    get props() {
+        return { hidden: true };
+    },
+
+    html: '<div hidden=""></div>',
+
+    async test({ props, target }) {
+        props.hidden = false;
+        await tick();
+        expect(target.innerHTML).toEqual("<div></div>");
+    },
 });
