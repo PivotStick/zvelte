31d8b9032fb1eb63a32d49328f61ca1b0dc42f3a
PivotStick <pivoorigami@gmail.com>
2024-06-15 01:32:45 +0200

    test: 🧪 attribute-dynamic-shorthand

--- src/__tests__/runtime-browser/samples/attribute-dynamic-shorthand/_config.js
+++ src/__tests__/runtime-browser/samples/attribute-dynamic-shorthand/_config.js
@@ -1,5 +1,17 @@
+import { expect } from "vitest";
+import { tick } from "../../../../internal/client/index.js";
 import { defineTest } from "../../defineTest.js";
 
 export default defineTest({
-    todo: true,
+    get props() {
+        return { id: "foo" };
+    },
+
+    html: '<div id="foo"></div>',
+
+    async test({ props, target }) {
+        props.id = "bar";
+        await tick();
+        expect(target.innerHTML).toEqual('<div id="bar"></div>');
+    },
 });
