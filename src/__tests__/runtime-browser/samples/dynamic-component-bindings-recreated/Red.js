96894b86e10f7e67eae68b74199657495aab37e7
PivotStick <pivoorigami@gmail.com>
2024-06-15 18:48:50 +0200

    test: 🧪 dynamic-component-bindings-recreated

---
+++ src/__tests__/runtime-browser/samples/dynamic-component-bindings-recreated/Red.js
@@ -0,0 +1,6 @@
+/**
+ * @param {import("@pivotass/zvelte").Args<{ foo: string }>} args
+ */
+export default function init({ props }) {
+    props.foo ??= "red";
+}
---
+++ src/__tests__/runtime-browser/samples/dynamic-component-bindings-recreated/_config.js
@@ -0,0 +1,39 @@
+import { expect } from "vitest";
+import { tick } from "../../../../internal/client/index.js";
+import { defineTest } from "../../defineTest.js";
+
+// @ts-expect-error
+import Green from "./Green.twig";
+// @ts-expect-error
+import Red from "./Red.twig";
+
+export default defineTest({
+    get props() {
+        return {
+            Green,
+            Red,
+            x: true,
+            foo: /** @type {any} */ ("green"),
+        };
+    },
+
+    html: "<p>parent green</p> <p>green green</p><!---->",
+
+    async test({ props, target }) {
+        props.foo = undefined;
+        props.x = false;
+        await tick();
+
+        expect(target.innerHTML).toEqual(
+            "<p>parent red</p> <p>red red</p><!---->"
+        );
+
+        props.foo = undefined;
+        props.x = true;
+        await tick();
+
+        expect(target.innerHTML).toEqual(
+            "<p>parent green</p> <p>green green</p><!---->"
+        );
+    },
+});
