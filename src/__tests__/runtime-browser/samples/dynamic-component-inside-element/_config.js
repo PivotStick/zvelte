111faa7430dda0bc1c4ed0430913f4a89e3b57e3
PivotStick <pivoorigami@gmail.com>
2024-06-16 17:24:42 +0200

    test: 🧪 dynamic-component-inside-element

---
+++ src/__tests__/runtime-browser/samples/dynamic-component-inside-element/_config.js
@@ -0,0 +1,29 @@
+import { expect } from "vitest";
+import { tick } from "../../../../internal/client/index.js";
+import { defineTest } from "../../defineTest.js";
+
+// @ts-expect-error
+import Foo from "./Foo.twig";
+// @ts-expect-error
+import Bar from "./Bar.twig";
+
+export default defineTest({
+    get props() {
+        return {
+            Foo,
+            Bar,
+            x: true,
+        };
+    },
+
+    html: "<div><p>true, therefore Foo</p><!----></div>",
+
+    async test({ props, target }) {
+        props.x = false;
+        await tick();
+
+        expect(target.innerHTML).toEqual(
+            "<div><p>false, therefore Bar</p><!----></div>"
+        );
+    },
+});
