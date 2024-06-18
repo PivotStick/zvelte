fc2e54c0b5c5f4f9ff564cdf9670f9a9b1c598ab
PivotStick <pivoorigami@gmail.com>
2024-06-18 19:08:48 +0200

    test: 🧪 attribute-dynamic-type

--- src/__tests__/runtime-browser/samples/attribute-dynamic-type/_config.js
+++ src/__tests__/runtime-browser/samples/attribute-dynamic-type/_config.js
@@ -1,5 +1,23 @@
-import { defineTest } from "../../defineTest.js";
+import { expect } from "vitest";
+import { defineTest, ok } from "../../defineTest.js";
+import { tick } from "../../../../internal/client/index.js";
 
 export default defineTest({
-    todo: true,
+    get props() {
+        return { inputType: "text", inputValue: 42 };
+    },
+
+    html: '<input type="text">',
+
+    async test({ props, target }) {
+        const input = target.querySelector("input");
+        ok(input);
+
+        expect(input.type).toEqual("text");
+        expect(input.value).toEqual("42");
+
+        props.inputType = "number";
+        await tick();
+        expect(input.type).toEqual("number");
+    },
 });
