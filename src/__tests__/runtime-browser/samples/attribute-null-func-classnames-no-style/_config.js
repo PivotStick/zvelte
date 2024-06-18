04a373d5b9a557a7946e42843280339a8334ee46
PivotStick <pivoorigami@gmail.com>
2024-06-18 20:02:49 +0200

    test: 🧪 attribute-null-func-classnames-no-style

--- src/__tests__/runtime-browser/samples/attribute-null-func-classnames-no-style/_config.js
+++ src/__tests__/runtime-browser/samples/attribute-null-func-classnames-no-style/_config.js
@@ -1,5 +1,62 @@
-import { defineTest } from "../../defineTest.js";
+import { expect } from "vitest";
+import { defineTest, ok } from "../../defineTest.js";
+import { tick } from "../../../../internal/client/index.js";
 
 export default defineTest({
-    todo: true,
+    get props() {
+        return {
+            /** @type {string | number | null | undefined} */
+            testName1: "test1",
+            /** @type {string | number | null | undefined} */
+            testName2: "test2",
+        };
+    },
+
+    html: '<div class="test1test2"></div>',
+
+    async test({ props, target }) {
+        const div = target.querySelector("div");
+        ok(div);
+        expect(div.className).toEqual("test1test2");
+
+        props.testName1 = null;
+        props.testName2 = null;
+        await tick();
+        expect(div.className).toEqual("nullnull");
+
+        props.testName1 = null;
+        props.testName2 = "test";
+        await tick();
+        expect(div.className).toEqual("nulltest");
+
+        props.testName1 = undefined;
+        props.testName2 = "test";
+        await tick();
+        expect(div.className).toEqual("undefinedtest");
+
+        props.testName1 = undefined;
+        props.testName2 = undefined;
+        await tick();
+        expect(div.className).toEqual("undefinedundefined");
+
+        props.testName1 = null;
+        props.testName2 = 1;
+        await tick();
+        expect(div.className).toEqual("null1");
+
+        props.testName1 = undefined;
+        props.testName2 = 1;
+        await tick();
+        expect(div.className).toEqual("undefined1");
+
+        props.testName1 = null;
+        props.testName2 = 0;
+        await tick();
+        expect(div.className).toEqual("null0");
+
+        props.testName1 = undefined;
+        props.testName2 = 0;
+        await tick();
+        expect(div.className).toEqual("undefined0");
+    },
 });
