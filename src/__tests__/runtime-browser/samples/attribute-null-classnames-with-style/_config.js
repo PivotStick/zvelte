8ec16b1a44616005415289c58123ad407ce5cca0
PivotStick <pivoorigami@gmail.com>
2024-06-18 19:54:57 +0200

    test: 🧪 attribute-null-classnames-with-style

--- src/__tests__/runtime-browser/samples/attribute-null-classnames-with-style/_config.js
+++ src/__tests__/runtime-browser/samples/attribute-null-classnames-with-style/_config.js
@@ -1,5 +1,62 @@
-import { defineTest } from "../../defineTest.js";
+import { defineTest, ok } from "../../defineTest.js";
+import { tick } from "../../../../internal/client/index.js";
+import { expect } from "vitest";
 
 export default defineTest({
-    todo: true,
+    get props() {
+        return {
+            /** @type {any} */
+            testName1: "test1",
+            /** @type {any} */
+            testName2: "test2",
+        };
+    },
+
+    html: '<div class="test1test2 zvelte-x1o6ra"></div>',
+
+    async test({ props, target }) {
+        const div = target.querySelector("div");
+        ok(div);
+        expect(div.className).toEqual("test1test2 zvelte-x1o6ra");
+
+        props.testName1 = null;
+        props.testName2 = null;
+        await tick();
+        expect(div.className).toEqual("nullnull zvelte-x1o6ra");
+
+        props.testName1 = null;
+        props.testName2 = "test";
+        await tick();
+        expect(div.className).toEqual("nulltest zvelte-x1o6ra");
+
+        props.testName1 = undefined;
+        props.testName2 = "test";
+        await tick();
+        expect(div.className).toEqual("undefinedtest zvelte-x1o6ra");
+
+        props.testName1 = undefined;
+        props.testName2 = undefined;
+        await tick();
+        expect(div.className).toEqual("undefinedundefined zvelte-x1o6ra");
+
+        props.testName1 = null;
+        props.testName2 = 1;
+        await tick();
+        expect(div.className).toEqual("null1 zvelte-x1o6ra");
+
+        props.testName1 = undefined;
+        props.testName2 = 1;
+        await tick();
+        expect(div.className).toEqual("undefined1 zvelte-x1o6ra");
+
+        props.testName1 = null;
+        props.testName2 = 0;
+        await tick();
+        expect(div.className).toEqual("null0 zvelte-x1o6ra");
+
+        props.testName1 = undefined;
+        props.testName2 = 0;
+        await tick();
+        expect(div.className).toEqual("undefined0 zvelte-x1o6ra");
+    },
 });
