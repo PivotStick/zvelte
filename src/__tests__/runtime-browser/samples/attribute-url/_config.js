7b49425393c0a93b4c9ae29acde98776fbea780b
PivotStick <pivoorigami@gmail.com>
2024-06-18 21:43:10 +0200

    test: 🧪 attribute-url

--- src/__tests__/runtime-browser/samples/attribute-url/_config.js
+++ src/__tests__/runtime-browser/samples/attribute-url/_config.js
@@ -2,15 +2,20 @@ import { expect } from "vitest";
 import { defineTest, ok } from "../../defineTest.js";
 
 export default defineTest({
-    todo: true,
+    get props() {
+        return {
+            bgImage: "https://example.com/foo.jpg",
+            color: "red",
+        };
+    },
 
     test({ target }) {
         const div = target.querySelector("div");
         ok(div);
 
         expect(div.style.backgroundImage).toEqual(
-            "url(https://example.com/foo.jpg)"
+            'url("https://example.com/foo.jpg")'
         );
-        expect(div.style.color).toEqual("lol");
+        expect(div.style.color).toEqual("red");
     },
 });
