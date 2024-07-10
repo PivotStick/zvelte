8f904d15c2df06bc528807e1253a0c2f5704f4bd
mdinatale <mdinatale@encaps.io>
2024-07-10 15:11:41 +0200

    override spread_props

--- src/internal/client/motion/index.d.ts
+++ src/internal/client/motion/index.d.ts
@@ -1,11 +1,11 @@
-export function tweened<T, K>(
+export function tweened<T, K extends keyof T>(
     props: T,
     key: K,
     value: T[K],
     opts: import("svelte/motion").TweenedOptions<T[K]>,
 ): import("svelte/motion").Tweened;
 
-export function spring<T, K>(
+export function spring<T, K extends keyof T>(
     props: T,
     key: K,
     value: T[K],
