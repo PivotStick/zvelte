257fa4dffa5113daa4bbd76b8f7caa43814df6fe
mdinatale <mdinatale@encaps.io>
2024-07-11 09:29:55 +0200

    correctly typed motion

--- src/internal/client/motion/index.d.ts
+++ src/internal/client/motion/index.d.ts
@@ -3,11 +3,11 @@ export function tweened<T, K extends keyof T>(
     key: K,
     value: T[K],
     opts?: import("svelte/motion").TweenedOptions<T[K]>,
-): import("svelte/motion").Tweened;
+): import("svelte/motion").Tweened<T[K]>;
 
 export function spring<T, K extends keyof T>(
     props: T,
     key: K,
     value: T[K],
     opts?: import("svelte/motion").SpringOpts,
-): import("svelte/motion").Spring;
+): import("svelte/motion").Spring<T[K]>;
