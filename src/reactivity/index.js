e0c1ab22d0a6370252262cd9b363fbc7d8e21a4c
mdinatale <mdinatale@encaps.io>
2024-09-26 16:33:34 +0200

    renamed reactivity

--- src/reactivity/index.js
+++ src/reactivity/index.js
@@ -1,1 +1,7 @@
-export * from "svelte/reactivity";
+export {
+    SvelteMap as ZvelteMap,
+    SvelteSet as ZvelteSet,
+    SvelteURL as ZvelteURL,
+    SvelteDate as ZvelteDate,
+    SvelteURLSearchParams as ZvelteURLSearchParams,
+} from "svelte/reactivity";
