a7b1b527126627884c7c67e9134274fccf6292d4
mdinatale <mdinatale@encaps.io>
2025-01-12 15:49:27 +0100

    handled optional render tag

--- src/compiler/phases/3-transform/render_dom/visitors/RenderTag.js
+++ src/compiler/phases/3-transform/render_dom/visitors/RenderTag.js
@@ -35,6 +35,15 @@ export function RenderTag(node, context) {
     );
 
     if (node.metadata.dynamic) {
+        // If we have a chain expression then ensure a nullish snippet function gets turned into an empty one
+        if (node.expression.optional) {
+            snippet_function = b.logical(
+                snippet_function,
+                "??",
+                b.id("$.noop"),
+            );
+        }
+
         context.state.init.push(
             b.stmt(
                 b.call(
