216674ea7490d868a9d506bf294b1a1f652e90c6
mdinatale <mdinatale@encaps.io>
2025-03-27 19:29:33 +0100

    hmr test

--- src/compiler/phases/3-transform/render_dom/index.js
+++ src/compiler/phases/3-transform/render_dom/index.js
@@ -30,6 +30,7 @@ import { VariableTag } from "./visitors/VariableTag.js";
 import { SnippetBlock } from "./visitors/SnippetBlock.js";
 import { RenderTag } from "./visitors/RenderTag.js";
 import { ForBlock } from "./visitors/ForBlock.js";
+import { arrow } from "../render_php_ssr/builders.js";
 
 /**
  * This function ensures visitor sets don't accidentally clobber each other
@@ -302,8 +303,6 @@ export function renderDom(source, ast, analysis, options, meta) {
      */
     const body = [...state.hoisted];
 
-    let exportDefaultId;
-
     if (options.async) {
         body.push(component);
 
@@ -352,11 +351,75 @@ export function renderDom(source, ast, analysis, options, meta) {
         if (Object.keys(exportSpecifiers).length) {
             body.push(b.exportSpecifiers(exportSpecifiers));
         }
-
-        exportDefaultId = load.id;
     } else {
-        body.push(b.exportDefault(component));
-        exportDefaultId = component.id;
+        if (options.hmr) {
+            body.push(b.stmt(b.call("$.mark_module_start")));
+            body.push(
+                b.stmt(
+                    b.assignment(
+                        "=",
+                        b.member(component.id, b.id("$.FILENAME"), true),
+                        b.string(options.filename),
+                    ),
+                ),
+            );
+            body.push(component);
+
+            const source = b.member(
+                b.member(component.id, b.id("$.HMR"), true),
+                b.id("source"),
+            );
+
+            body.push(
+                b.if(
+                    b.id("import.meta.hot"),
+                    b.block([
+                        b.stmt(
+                            b.assignment(
+                                "=",
+                                component.id,
+                                b.call("$.hmr", component.id, b.thunk(source)),
+                            ),
+                        ),
+                        b.stmt(
+                            b.call(
+                                "$.import.meta.hot.acceptExports",
+                                b.array([b.string("default")]),
+                                b.arrow(
+                                    [b.id("module")],
+                                    b.block([
+                                        b.stmt(
+                                            b.assignment(
+                                                "=",
+                                                b.id(
+                                                    "module.default[$.HMR].source",
+                                                ),
+                                                source,
+                                            ),
+                                        ),
+
+                                        b.stmt(
+                                            b.call(
+                                                "$.set",
+                                                source,
+                                                b.id(
+                                                    "module.default[$.HMR].original",
+                                                ),
+                                            ),
+                                        ),
+                                    ]),
+                                ),
+                            ),
+                        ),
+                    ]),
+                ),
+            );
+
+            body.push(b.exportDefault(component.id));
+            body.push(b.stmt(b.call("$.mark_module_end", component.id)));
+        } else {
+            body.push(b.exportDefault(component));
+        }
     }
 
     return print({
